3bccad5593f17dac1eaf13c6fa64e761889d8acb
Kailane Gomes de Sousa <124894214+KailaneGomesS@users.noreply.github.com>
2023-06-13 12:00:36 -0300

    Add files via upload

---
+++ Telas/src/telas/TelaInicial.java
@@ -0,0 +1,134 @@
+package telas;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class TelaInicial extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaInicial frame = new TelaInicial();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaInicial() {
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu Menu = new JMenu("MENU");
+		Menu.setHorizontalAlignment(SwingConstants.CENTER);
+		menuBar.add(Menu);
+		
+		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Modo de Uso");
+		mntmNewMenuItem_3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				infoInicial info= new infoInicial ();
+		          info.setVisible(true);
+			
+			}
+		});
+		Menu.add(mntmNewMenuItem_3);
+		
+	
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Sobre a equipe");
+		mntmNewMenuItem_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Sobre sEquipe= new Sobre ();
+		          sEquipe.setVisible(true);
+				
+			}
+		});
+		Menu.add(mntmNewMenuItem_2);
+		
+		JMenuItem menuSair= new JMenuItem("Sair");
+		menuSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				
+			}
+		});
+		
+		
+	
+		menuSair.setHorizontalAlignment(SwingConstants.CENTER);
+		Menu.add(menuSair);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Calculadora IAC");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				iac calculador= new iac();
+		          calculador.setVisible(true);
+			}
+		});
+		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 21));
+		btnNewButton.setBounds(65, 90, 287, 36);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Escolha uma Calculadora ");
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 23));
+		lblNewLabel.setBounds(76, 28, 287, 36);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnCalculadoraImc = new JButton("Calculadora IMC");
+		btnCalculadoraImc.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				imc calcIMC= new imc ();
+		          calcIMC.setVisible(true);
+			}
+		});
+		btnCalculadoraImc.setFont(new Font("Times New Roman", Font.BOLD, 21));
+		btnCalculadoraImc.setBounds(65, 138, 287, 36);
+		contentPane.add(btnCalculadoraImc);
+	}
+
+}
---
+++ Telas/src/telas/infoIMC.java
@@ -0,0 +1,96 @@
+package telas;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
+public class infoIMC extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					infoIMC frame = new infoIMC();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public infoIMC() {
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		
+		JMenu Menu = new JMenu("MENU");
+		Menu.setHorizontalAlignment(SwingConstants.CENTER);
+		menuBar.add(Menu);
+		
+
+		
+		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Tela Inicial");
+		mntmNewMenuItem_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+					TelaInicial escolha= new TelaInicial ();
+			          escolha.setVisible(true);
+				
+			}
+		});
+		Menu.add(mntmNewMenuItem_1);
+		
+		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Sobre a equipe");
+		mntmNewMenuItem_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Sobre sEquipe= new Sobre ();
+		          sEquipe.setVisible(true);
+				
+			}
+		});
+		Menu.add(mntmNewMenuItem_2);
+		
+		JMenuItem menuSair= new JMenuItem("Sair");
+		menuSair.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+				
+			}
+		});
+		
+		
+	
+		menuSair.setHorizontalAlignment(SwingConstants.CENTER);
+		Menu.add(menuSair);
+		
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 583, 472);
+		getContentPane().setLayout(null);
+		
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
